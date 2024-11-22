7220d305cd6160fe6f6b4e4ed6a58af5bf19a666
Álvaro Croce <alvaro@retailsystem.com>
2024-11-22 13:31:26 -0300

    name refactor

--- Ch3/L02_iterations.py
+++ Ch3/L02_iterations.py
@@ -2,8 +2,8 @@ nums = [1,5,8,10,7,2]
 
 # for in is similar to foreach in c#
 for n in nums:
-  print(f"The value is {n}")
+    print(f"The value is {n}")
 
 # enumerate() function allows you to loop over a list and have an automatic counter
 for idx, n in enumerate(nums, start=1):
-  print(f"Index {idx} has value {n}")
+    print(f"Index {idx} has value {n}")
