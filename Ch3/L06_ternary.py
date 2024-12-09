0d805e6e02199004ce8980efbd9904698409fe35
Álvaro Croce <amcroce@outlook.com>
2024-12-09 11:20:20 -0300

    Minor fixes

--- Ch3/L06_ternary.py
+++ Ch3/L06_ternary.py
@@ -2,13 +2,14 @@ def main():
     while True:
         text = input("Enter a number 1 -> 1.000 (blank to exit): ")
         ## Check if text has a value
-        if not text or len(text.strip()) == 0:
+        ## not evaluates as Ture if text is empty, because of the "falsy" concept
+        if not text.strip():
             print("Later...")
             break
         ## Check if text is a number
         num = int(text)
-        numClass = "small" if num < 100 else "huge!"
-        print(f"The number {num} is {numClass}")
+        num_class = "small" if num < 100 else "huge!"
+        print(f"The number {num} is {num_class}")
 
 if __name__ == "__main__":
     main()
