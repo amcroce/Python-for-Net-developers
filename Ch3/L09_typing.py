0201bb95d096316fadbe82804bd799105cfc660b
Álvaro Croce <amcroce@outlook.com>
2024-12-13 12:42:57 -0300

    With static method

--- Ch3/L09_typing.py
+++ Ch3/L09_typing.py
@@ -1,11 +1,22 @@
 class Wizard:
-    def __init__(self, name, level):
-        self.name = name
-        self.level = level
+    # def __init__(self, name, level):
+    #     self.name = name
+    #     self.level = level
+
+    def __init__(self):
+        self.name = "Gandalf"
+        self.level = 0
 
     @classmethod
-    def train(cls, baseLevel):
-        return cls("Gandalf", baseLevel)
+    def train_with_class_method(cls, baseLevel):
+        return cls("Gandalf", baseLevel+1)
+
+    @staticmethod
+    def train(baseLevel):
+        w = Wizard()
+        w.level += baseLevel
+        return w
+
 
 def main():
     gandalf = Wizard.train(10)
